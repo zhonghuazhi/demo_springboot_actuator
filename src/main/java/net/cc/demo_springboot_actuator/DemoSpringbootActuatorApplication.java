461fa3828abad8f6904daf58559c655cb911da21
zhizhonghua <zhizhonghua@boco.com.cn>
2019-04-12 10:22:58 +0800

    版本初始化。

---
+++ src/main/java/net/cc/demo_springboot_actuator/DemoSpringbootActuatorApplication.java
@@ -0,0 +1,13 @@
+package net.cc.demo_springboot_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootActuatorApplication.class, args);
+    }
+
+}
